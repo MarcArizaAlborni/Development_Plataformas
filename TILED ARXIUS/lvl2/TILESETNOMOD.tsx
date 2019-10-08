9466dcd1f355bc43b7da950c3cce3dbc8f14ee16
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2019-10-08 23:35:00 +0200

    tot be

---
+++ TILED ARXIUS/lvl2/TILESETNOMOD.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tileset" tilewidth="32" tileheight="32" tilecount="48" columns="12">
+ <image source="tileset.png" width="384" height="144"/>
+</tileset>
