3696efccb1f4b009e26001e60f379dbc704a2e31
Marc Ariza Alborni <ariza.marc1999@gmail.com>
2019-10-10 10:32:37 +0200

    Revert "hola"
    This reverts commit e3f7fbcb48a0ab20da7dbf3871c37152e2699938.

--- Dev04_Handout/Motor2D/j1FadeToBlack.cpp
+++ Dev04_Handout/Motor2D/j1FadeToBlack.cpp
@@ -9,7 +9,7 @@
 j1FadeToBlack::j1FadeToBlack()
 {
 	
-	//screen = { 0, 0, SCREEN_WIDTH * SCREEN_SIZE, SCREEN_HEIGHT * SCREEN_SIZE };
+	screen = { 0, 0, SCREEN_WIDTH * SCREEN_SIZE, SCREEN_HEIGHT * SCREEN_SIZE };
 }
 
 j1FadeToBlack::~j1FadeToBlack()
--- Dev04_Handout/Motor2D/j1Map.cpp
+++ Dev04_Handout/Motor2D/j1Map.cpp
@@ -48,8 +48,6 @@ void j1Map::Draw()
 		}
 	}
 	// TODO 9: Complete the draw function
-
-
 }
 
 
