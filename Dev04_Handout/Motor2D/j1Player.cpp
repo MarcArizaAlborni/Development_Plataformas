6cc9ed655bbf7b45a9f4e3fbd065b865cc6357e6
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2019-10-10 16:02:10 +0200

    Player Modules

---
+++ Dev04_Handout/Motor2D/j1Player.cpp
@@ -0,0 +1,95 @@
+#include "j1Module.h"
+#include "j1App.h"
+#include "j1Player.h"
+#include "j1Render.h"
+#include "j1Input.h"
+#include "p2Point.h"
+
+
+j1Player::j1Player(){
+
+	
+	
+
+}
+
+
+ j1Player::~j1Player() {
+
+
+
+}
+
+
+
+ bool j1Player::Init() {
+
+
+
+	 return 0;
+ }
+
+
+ bool j1Player::Awake(pugi::xml_node config) {
+
+
+
+ }
+
+
+
+ bool j1Player::Start() {
+
+
+
+
+	 return 0;
+ }
+
+ bool j1Player::PreUpdate() {
+
+
+
+
+
+
+	 return 0;
+
+
+ }
+
+
+ bool j1Player::Update() {
+
+	 switch (CurrentState)
+	 {
+	 case idle:
+	 
+
+		 break;
+
+
+	 default:
+		 break;
+	 }
+
+
+	 return 0;
+ }
+
+ bool j1Player::PostUpdate() {
+
+
+
+	 return 0;
+
+ }
+
+
+
+ bool j1Player::CleanUp() {
+
+
+
+
+ }
--- Dev04_Handout/Motor2D/j1Player.h
+++ Dev04_Handout/Motor2D/j1Player.h
@@ -1,1 +1,52 @@
 #pragma once
+
+#include "j1Module.h"
+#include "j1Input.h"
+#include "j1Render.h"
+#include "p2Point.h"
+
+
+enum CurrentState {
+
+	idle,
+	jump, 
+	move_left,
+	move_right
+
+
+
+
+};
+class j1Player : public j1Module {
+private:
+	
+	int floor_Level = 0; //Ni idea del valor nomes poso algo per posarho
+	float speed_char = 2;  //Ni idea del valor nomes poso algo per posarho
+	SDL_Rect Player_Rect;
+public:
+
+	j1Player();
+
+	// Destructor
+	virtual ~j1Player();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node config);
+
+	bool Init();
+
+	bool Start();
+
+	
+
+	bool PreUpdate();
+	
+	bool Update();
+
+	bool PostUpdate();
+
+	// Called before quitting
+	bool CleanUp();
+
+
+};
