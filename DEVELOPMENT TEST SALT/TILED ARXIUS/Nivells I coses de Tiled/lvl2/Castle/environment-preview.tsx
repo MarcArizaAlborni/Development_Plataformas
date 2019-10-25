5241fbcc2b97300444cb3feb17e54e3fc3e5d0cd
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2019-10-25 23:43:02 +0200

    test del salt, es per no liarla amb el salt d'abans i el que has fet

---
+++ DEVELOPMENT TEST SALT/TILED ARXIUS/Nivells I coses de Tiled/lvl2/Castle/environment-preview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="environment-preview" tilewidth="32" tileheight="32" tilecount="363" columns="33">
+ <image source="warped-files/PNG/environment/environment-preview.png" width="1056" height="352"/>
+</tileset>
