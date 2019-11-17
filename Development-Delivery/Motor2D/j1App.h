32221850e1792e97608e1760320a3638b952ebdc
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2019-11-17 13:12:38 +0100

    timer fet

--- Development-Delivery/Motor2D/j1App.h
+++ Development-Delivery/Motor2D/j1App.h
@@ -4,6 +4,8 @@
 #include "p2List.h"
 #include "j1Module.h"
 #include "PugiXml\src\pugixml.hpp"
+#include "j1Timer.h"
+#include "j1PerfTimer.h"
 
 // Modules
 class j1Window;
@@ -110,6 +112,22 @@ private:
 	bool				want_to_load;
 	p2SString			load_game;
 	mutable p2SString	save_game;
+
+
+
+	//Framerate
+	uint64				frame_count;			//Calcultes the amount of frames per second. frames++ after each Update() loop.
+	j1Timer				startup_timer;			//Creates a pointer to j1Timer tool. Gives acces to j1Timer's elements. Used to keep track of time since app start.
+	j1Timer				frame_timer;			//Keeps track of everything time related in the span of a frame.
+	j1PerfTimer			perf_timer;				//Creates a pointer to j1PerfTimer tool. Gives access to j1PerfTimer's elements. Used to keep track of time since app start.
+	j1PerfTimer			last_second_timer;		//Creates a pointer to j1PerfTimer tool. Used to calculate variables in spans of one second.
+	uint32				last_update_ms;			//Calculates the amount of milliseconds that the last update spent running.
+	uint32				frames_last_second;		//Calculates the amount of frames that where processed the last second.
+	uint32				prev_sec_frames;		//Stores the number of frames processed last second.
+
+	j1PerfTimer			true_delay_timer;		//Timer that will be used to see the actual amount of time that was delayed to cap the framerate.
+	uint				frame_cap;				//Stores the frames per second cap to be applied.
+
 };
 
 extern j1App* App; // No student is asking me about that ... odd :-S
