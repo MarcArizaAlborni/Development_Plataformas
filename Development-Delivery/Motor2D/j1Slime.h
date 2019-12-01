b23a401185541c45345fa3cdd78242748fecccae
MarcArizaAlborni <45202074+MarcArizaAlborni@users.noreply.github.com>
2019-12-01 17:51:22 +0100

    skull i slime

--- Development-Delivery/Motor2D/j1Slime.h
+++ Development-Delivery/Motor2D/j1Slime.h
@@ -1,5 +1,10 @@
 #include "p2Point.h"
 #include "j1Entities.h"
+#include "p2DynArray.h"
+
+#define DETECTION_RANGE 150
+#define NEGATIVE_DETECTION_RANGE -150
+
 
 struct Animation;
 enum EntitiesType;
@@ -37,4 +42,33 @@ public:
 	Animation		walking;
 	Animation		death;
 	Animation		attack;
+	
+
+
+
+
+	//PATHING
+	//p2DynArray<iPoint>* path;
+	bool Dead;
+	float gravity;
+	float jumpforce_xml;
+	iPoint Pathing;
+
+	bool GoLeft;
+	bool GoRight;
+	iPoint LastPosition;
+	bool TouchingColliderLeft; //PER L' ESQUERRA TOCA COLLIDER
+	bool TouchingColliderRight;//PERLA DRETA TOCA COLLIDER
+
+
+	bool AttackLeft;
+	bool AttackRight;
+
+	//PATHING FUNCTION
+//	void LookForPath();
+	//void j1Skeleton::Move(p2DynArray<iPoint>& path, float dt);
+
+	void j1Slime::ComparePositions();
+	void j1Slime::GroundJump();
+	void j1Slime::Movement();
 };
